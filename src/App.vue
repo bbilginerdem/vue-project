5ebf102329fe3bf2e5fa9022548bd07a6c1b19eb
Behzat Bilgin Erdem <bbilgin.erdem@gmail.com>
2023-11-07 19:53:02 +0300

    top half mostly done with responsiveness

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,9 @@
 <script setup>
-// import { RouterView, RouterLink  } from 'vue-router'
-// 
-
 import TheWelcome from './components/TheWelcome.vue';
-
-// import WelcomeItem from './components/WelcomeItem.vue';
-
-// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
+<template class="font-generalSans">
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
     <TheWelcome />
-    <!-- <div class="absolute mt-0"> -->
-      <!-- <HelloWorld msg="You did it!" /> -->
-      <!-- <div class="mt-0">xd</div> -->
-      <!-- <div>xd</div> -->
-      <!-- <div>sdfa</div> -->
-      <!-- <WelcomeItem /> -->
-      <!-- <nav> -->
-        <!-- <RouterLink to="/">Home</RouterLink> -->
-        <!-- <RouterLink to="/about">About</RouterLink> -->
-      <!-- </nav> -->
-    <!-- </div> -->
   </header>
-
-  <!-- <RouterView /> -->
 </template>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,209 +1,129 @@
 <script setup>
 // import WelcomeItem from './WelcomeItem.vue'
-// import DocumentationIcon from './icons/IconDocumentation.vue'
+// import Edit from './icons/ant-design_edit-outlined.svg'
 // import ToolingIcon from './icons/IconTooling.vue'
 // import EcosystemIcon from './icons/IconEcosystem.vue'
 // import CommunityIcon from './icons/IconCommunity.vue'
 // import SupportIcon from './icons/IconSupport.vue'
 </script>
 
+<!-- <div>
+  <img src="./icons/ant-design_edit-outlined.svg" class="w-10 h-10 bg-black" />
+</div> -->
+
 <template>
-  <div class="bg-white">
-    <div class="w-8 h-8 left-[264px] top-[199px] absolute"></div>
-    <img class="w-[152px] h-[152px] left-[75px] top-[105px] absolute rounded-full border border-slate-300"
-      src="https://via.placeholder.com/152x152" />
-    <div
-      class="left-[270px] top-[121px] absolute text-zinc-600 text-3xl font-medium font-['General Sans'] leading-[30px]">
-      Fatih Keskin</div>
-    <div
-      class="left-[270px] top-[156px] absolute text-zinc-600 text-[15px] font-normal font-['General Sans'] leading-[30px]">
-      Keskin Soğutma ve Beyaz Eşya</div>
-    <div
-      class="left-[93px] top-[320px] absolute text-zinc-500 text-[13px] font-normal font-['General Sans'] leading-[30px]">
-      26 kişi değerlendirdi</div>
-    <div class="left-[304px] top-[283px] absolute"><span
-        style="text-zinc-500 text-[13px] font-medium font-['General Sans'] leading-[30px]">290</span><span
-        style="text-zinc-500 text-[13px] font-normal font-['General Sans'] leading-[30px]"> kez hizmet verdi</span></div>
-    <div
-      class="left-[599px] top-[283px] absolute text-zinc-500 text-[13px] font-normal font-['General Sans'] leading-[30px]">
-      Onaylanmış Belgeler</div>
-    <div
-      class="left-[585px] top-[320px] absolute text-zinc-500 text-[13px] font-medium font-['General Sans'] leading-[30px]">
-      Kimlik</div>
-    <div
-      class="left-[585px] top-[349px] absolute text-zinc-500 text-[13px] font-medium font-['General Sans'] leading-[30px]">
-      Vergi Levhası</div>
-    <div
-      class="left-[585px] top-[377px] absolute text-zinc-500 text-[13px] font-medium font-['General Sans'] leading-[30px]">
-      Cep Telefonu</div>
-    <div
-      class="left-[586px] top-[407px] absolute text-zinc-500 text-[13px] font-medium font-['General Sans'] leading-[30px]">
-      E-Posta</div>
-    <div
-      class="left-[304px] top-[191px] absolute text-zinc-500 text-[13px] font-normal font-['General Sans'] leading-[30px]">
-      Atatürk mahallesi, Sakarya sokağı, No: 13/5</div>
-    <div
-      class="left-[304px] top-[209px] absolute text-zinc-500 text-[13px] font-medium font-['General Sans'] leading-[30px]">
-      Ataşehir / Anadolu Yakası / İstanbul</div>
-    <div class="left-[270px] top-[91px] absolute"><span
-        style="text-blue-500 text-[13px] font-medium font-['General Sans'] leading-[30px]">Kasım 2021</span><span
-        style="text-blue-500 text-[13px] font-normal font-['General Sans'] leading-[30px]">’den beri üye</span></div>
-    <div
-      class="left-[145px] top-[286px] absolute text-blue-500 text-[40px] font-medium font-['General Sans'] leading-[30px]">
-      4.3</div>
-    <div
-      class="left-[304px] top-[320px] absolute text-blue-500 text-[15px] font-medium font-['General Sans'] leading-[30px]">
-      Hız</div>
-    <div
-      class="left-[304px] top-[400px] absolute text-blue-500 text-[15px] font-medium font-['General Sans'] leading-[30px]">
-      Kalite</div>
-    <div
-      class="left-[304px] top-[360px] absolute text-blue-500 text-[15px] font-medium font-['General Sans'] leading-[30px]">
-      Fiyat</div>
-    <div
-      class="left-[304px] top-[440px] absolute text-blue-500 text-[15px] font-medium font-['General Sans'] leading-[30px]">
-      Nezaket</div>
-    <div class="w-[152px] h-[42px] left-[75px] top-[364px] absolute">
-      <div class="w-[152px] h-[42px] left-0 top-0 absolute bg-blue-500 rounded-[10px]"></div>
-      <div class="w-[19px] h-[19px] left-[22px] top-[12px] absolute"></div>
-      <div class="left-[50px] top-[6px] absolute text-white text-[15px] font-normal font-['General Sans'] leading-[30px]">
-        Değerlendir</div>
-    </div>
-    <div class="w-[152px] h-[42px] left-[75px] top-[420px] absolute">
-      <div class="w-[152px] h-[42px] left-0 top-0 absolute bg-green-600 rounded-[10px]"></div>
-      <div class="left-[50px] top-[6px] absolute text-white text-[15px] font-normal font-['General Sans'] leading-[30px]">
-        Hemen Ara</div>
-      <div class="w-5 h-[21px] left-[23px] top-[13px] absolute"></div>
-    </div>
-    <div class="left-[678px] top-[568px] absolute text-blue-500 text-xl font-medium font-['General Sans'] leading-[30px]">
-      Galeri</div>
-    <div class="left-[75px] top-[568px] absolute"><span
-        style="text-blue-500 text-xl font-normal font-['General Sans'] leading-[30px]">Hizmet Verdiği</span><span
-        style="text-blue-500 text-xl font-medium font-['General Sans'] leading-[30px]"> Sektörler</span></div>
-    <div class="left-[75px] top-[851px] absolute"><span
-        style="text-blue-500 text-xl font-normal font-['General Sans'] leading-[30px]">Hizmet Verdiği</span><span
-        style="text-blue-500 text-xl font-medium font-['General Sans'] leading-[30px]"> İlçeler</span></div>
-    <div class="w-[597px] h-[371px] left-[785px] top-[91px] absolute">
-      <div class="w-[597px] h-[371px] left-0 top-0 absolute">
-        <div class="w-[597px] h-[371px] left-0 top-0 absolute bg-slate-100 rounded-xl"></div>
-        <div class="w-36 h-[58px] left-0 top-0 absolute">
-          <div class="w-36 h-[58px] left-0 top-0 absolute bg-white rounded-tl-xl rounded-br-xl"></div>
-          <div
-            class="left-[28px] top-[14px] absolute text-blue-500 text-xl font-medium font-['General Sans'] leading-[30px]">
-            Hakkında</div>
+  <div class="flex flex-col xl:flex-row">
+    <!-- Top Left -->
+    <div>
+      <!-- First -->
+      <div class="flex xl:p-10 md:p-14 max-xl:justify-center">
+        <div>
+          <img src="./images/fix-store.png" class="" />
+        </div>
+        <div>
+          <div class="text-blue-500 text-[13px] font-sans font-medium leading-[30px]">
+            Kasım 2021’den beri üye
+          </div>
+          <div class="font-bold">
+            Fatih Keskin
+          </div>
+          <div>Keskin Soğutma ve Beyaz Eşya</div>
+          <div class="flex flex-row">
+            <img src="./icons/carbon_location.svg" class="" />
+            <div>
+              <div class="">Atatürk mahallesi, Sakarya sokağı, No: 13/5</div>
+              <div class="">Ataşehir / Anadolu Yakası / İstanbul</div>
+            </div>
+          </div>
         </div>
       </div>
-      <div
-        class="w-[511px] left-[49px] top-[94px] absolute text-zinc-600 text-[15px] font-normal font-['General Sans'] leading-[30px]">
+      <!-- Second -->
+      <div class="flex max-md:flex-col md:flex-1 max-xl:justify-center">
+        <div class="m-10 max-md:mx-auto">
+          <div class="flex flex-row">
+            <img src="./icons/tabler_star.svg" class="" />
+            <div>4.3</div>
+          </div>
+          <span>26 kişi değerlendirdi</span>
+          <button class="h-[42px] w-[152px] bg-blue-500 flex items-center justify-center rounded-[10px]"><img
+              src="./icons/ant-design_edit-outlined.svg" class="" />Değerlendir</button>
+          <button class="w-[152px] h-[42px] bg-green-600 flex items-center justify-center rounded-[10px]"><img
+              src="./icons/solar_phone-bold.svg" class="" />Hemen Ara</button>
+        </div>
+        <!-- Vertical Line Break -->
+        <div class="hidden md:inline-block h-[250px] min-h-[1em] w-0.5 self-stretch bg-slate-300 opacity-100"></div>
+        <hr class="md:hidden my-6 h-0.5 border-t-0 mx-32 bg-slate-300 opacity-100 dark:opacity-50" />
+        <div class="m-10 max-md:mx-auto">
+          <div class="">
+            290 kez hizmet verdi
+          </div>
+          <div class="flex flex-row items-center">
+            Hız
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+          </div>
+          <div class="flex flex-row items-center">
+            Fiyat
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+          </div>
+          <div class="flex flex-row items-center">
+            Kalite
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+          </div>
+          <div class="flex flex-row items-center">
+            Hız
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+          </div>
+          <div class="flex flex-row items-center">
+            Nezaket
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+            <img src="./icons/tabler_star.svg" />
+          </div>
+        </div>
+        <!-- Vertical Line Break -->
+        <div class="hidden md:inline-block md:h-[250px] min-h-[1em] w-0.5 self-stretch bg-slate-300 opacity-100"></div>
+        <hr class="md:hidden my-6 h-0.5 border-t-0 mx-32 bg-slate-300 opacity-100 dark:opacity-50" />
+        <div class="m-10 max-md:mx-auto">
+          <div class="flex flex-row">
+            <img src="./icons/material-symbols-light_verified-rounded.svg" class="" />
+            <span>Onaylanmış Belgeler</span>
+          </div>
+          <ul class="list-disc text-blue-500 text-[13px] font-medium leading-[30px]">
+            <li class="text-zinc-500">Kimlik</li>
+            <li class="text-zinc-500">Vergi Levhası</li>
+            <li class="text-zinc-500">Cep Telefonu</li>
+            <li class="text-zinc-500">E-Posta</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- Top Right -->
+    <div class="w-[597px] h-[371px] bg-slate-100 rounded-xl m-10 max-xl:justify-center max-xl:items-center mx-auto">
+      <div class="bg-white w-20 rounded-md">Hakkında</div>
+      <div class="m-10">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
         magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </div>
     </div>
-    <div class="w-[405.36px] left-[75px] top-[625px] absolute justify-start items-start gap-[15px] inline-flex">
-      <div class="px-[16.71px] py-[4.56px] bg-blue-500 rounded-lg justify-center items-center gap-[7.59px] flex">
-        <div class="text-center text-white text-xs font-medium font-['General Sans'] leading-snug">Beyaz Eşya Servisi
-        </div>
-      </div>
-      <div
-        class="px-[18.23px] py-[4.56px] rounded-lg border border-slate-300 justify-center items-center gap-[7.59px] flex">
-        <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-snug">Çamaşır Makinesi
-          Tamiri</div>
-      </div>
-      <div
-        class="px-[18.23px] py-[4.56px] rounded-lg border border-slate-300 justify-center items-center gap-[7.59px] flex">
-        <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-snug">Fırın Tamiri</div>
-      </div>
-      <div
-        class="px-[18.23px] py-[4.56px] rounded-lg border border-slate-300 justify-center items-center gap-[7.59px] flex">
-        <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-snug">Davlumbaz Tamiri
-        </div>
-      </div>
-      <div
-        class="px-[18.23px] py-[4.56px] rounded-lg border border-slate-300 justify-center items-center gap-[7.59px] flex">
-        <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-snug">Buzdolabı Tamiri
-        </div>
-      </div>
-      <div
-        class="px-[18.23px] py-[4.56px] rounded-lg border border-slate-300 justify-center items-center gap-[7.59px] flex">
-        <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-snug">Bulaşık Makinesi
-          Tamiri</div>
-      </div>
-      <div
-        class="px-[18.23px] py-[4.56px] rounded-lg border border-slate-300 justify-center items-center gap-[7.59px] flex">
-        <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-snug">Küçük Ev Aletleri
-          Tamiri</div>
-      </div>
-      <div
-        class="px-[18.23px] py-[4.56px] rounded-lg border border-slate-300 justify-center items-center gap-[7.59px] flex">
-        <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-snug">Buzdolabı Tamiri
-        </div>
-      </div>
-      <div
-        class="px-[18.23px] py-[4.56px] rounded-lg border border-slate-300 justify-center items-center gap-[7.59px] flex">
-        <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-snug">Bulaşık Makinesi
-          Tamiri</div>
-      </div>
-    </div>
-    <div class="w-[468.35px] left-[75px] top-[907px] absolute justify-start items-start gap-4 inline-flex">
-      <div class="w-[80.87px] h-[33.12px] bg-blue-500 rounded-lg"></div>
-      <div class="w-[80.87px] h-[33.12px] rounded-lg border border-slate-300"></div>
-      <div class="w-[80.87px] h-[33.12px] rounded-lg border border-slate-300"></div>
-      <div class="text-center text-white text-xs font-medium font-['General Sans'] leading-[23.11px]">Ataşehir</div>
-      <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-[23.11px]">Beyoğlu</div>
-      <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-[23.11px]">Kartal</div>
-      <div class="w-[80.87px] h-[33.12px] rounded-lg border border-slate-300"></div>
-      <div class="w-[80.87px] h-[33.12px] rounded-lg border border-slate-300"></div>
-      <div class="w-[80.87px] h-[33.12px] rounded-lg border border-slate-300"></div>
-      <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-[23.11px]">Maltepe</div>
-      <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-[23.11px]">Maltepe</div>
-      <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-[23.11px]">Şişli</div>
-      <div class="w-[80.87px] h-[33.12px] rounded-lg border border-slate-300"></div>
-      <div class="w-[80.87px] h-[33.12px] rounded-lg border border-slate-300"></div>
-      <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-[23.11px]">Kadıköy</div>
-      <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-[23.11px]">Pendik</div>
-      <div class="w-[80.87px] h-[33.12px] rounded-lg border border-slate-300"></div>
-      <div class="w-[80.87px] h-[33.12px] rounded-lg border border-slate-300"></div>
-      <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-[23.11px]">Beşiktaş</div>
-      <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-[23.11px]">Üsküdar</div>
-      <div class="w-[80.87px] h-[33.12px] rounded-lg border border-slate-300"></div>
-      <div class="w-[80.87px] h-[33.12px] rounded-lg border border-slate-300"></div>
-      <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-[23.11px]">Beşiktaş</div>
-      <div class="text-center text-blue-500 text-xs font-normal font-['General Sans'] leading-[23.11px]">Üsküdar</div>
-    </div>
-    <div class="w-[706px] left-[678px] top-[625px] absolute justify-start items-start gap-[18px] inline-flex">
-      <img class="w-[163px] h-[163px] rounded-xl" src="https://via.placeholder.com/163x163" />
-      <img class="w-[163px] h-[163px] rounded-xl" src="https://via.placeholder.com/163x163" />
-      <img class="w-[163px] h-[163px] rounded-xl" src="https://via.placeholder.com/163x163" />
-      <div class="w-[163px] h-[163px] relative">
-        <img class="w-[163px] h-[163px] left-0 top-0 absolute rounded-xl" src="https://via.placeholder.com/163x163" />
-        <div class="w-[163px] h-[163px] left-0 top-0 absolute bg-zinc-900 rounded-xl"></div>
-        <div class="w-11 h-11 left-[60px] top-[60px] absolute"></div>
-      </div>
-      <img class="w-[163px] h-[163px] rounded-xl" src="https://via.placeholder.com/163x163" />
-      <img class="w-[163px] h-[163px] rounded-xl" src="https://via.placeholder.com/163x163" />
-    </div>
-    <div class="w-6 h-6 left-[565px] top-[286px] absolute"></div>
-    <div class="w-2.5 h-2.5 left-[568px] top-[331px] absolute">
-      <div class="w-2.5 h-2.5 left-0 top-0 absolute">
-      </div>
-    </div>
-    <div class="w-2.5 h-2.5 left-[568px] top-[360px] absolute">
-      <div class="w-2.5 h-2.5 left-0 top-0 absolute">
-      </div>
-    </div>
-    <div class="w-2.5 h-2.5 left-[568px] top-[388px] absolute">
-      <div class="w-2.5 h-2.5 left-0 top-0 absolute">
-      </div>
-    </div>
-    <div class="w-2.5 h-2.5 left-[568px] top-[417px] absolute">
-      <div class="w-2.5 h-2.5 left-0 top-0 absolute">
-      </div>
-    </div>
-    <div class="left-[252px] top-[778px] absolute text-blue-500 text-sm font-medium font-['General Sans'] leading-tight">
-      Daha fazla yükle</div>
-    <div class="left-[252px] top-[1070px] absolute text-blue-500 text-sm font-medium font-['General Sans'] leading-tight">
-      Daha fazla yükle</div>
   </div>
 </template>
